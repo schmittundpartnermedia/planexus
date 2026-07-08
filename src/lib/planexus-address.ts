9cc432eadc19499db8807d501cc71446676d5d37
Joachim Schmitt <schmittundpartnermedia@gmail.com>
2026-07-08 23:03:16 +0200

    Kritische SEO-Fixes: LocalBusiness Adresse, Schema-Duplikate entfernt

---
+++ src/lib/planexus-address.ts
@@ -0,0 +1,19 @@
+export const planexusPostalAddress = {
+  "@type": "PostalAddress",
+  streetAddress: "Am Steinbach 8",
+  postalCode: "72459",
+  addressLocality: "Albstadt-Laufen",
+  addressRegion: "Baden-Württemberg",
+  addressCountry: "DE",
+} as const;
+
+export const planexusGeo = {
+  "@type": "GeoCoordinates",
+  latitude: 48.2180461,
+  longitude: 8.9140541,
+} as const;
+
+export const planexusContact = {
+  telephone: "+49 7435 7519 700",
+  email: "info@planexus.de",
+} as const;
